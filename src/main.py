2321e368d3120b6a4ca3ff370d235b3651930839
unknown <anandmanash321@gmail.com>
2024-07-11 00:00:34 +0530

    learn about motor lifespan and asynccontextmanage

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI 
+from .db.utils import get_mongoDb
 
 
 @asynccontextmanager
 async def lifeSpan(app:FastAPI):
-    
+    mongodb = get_mongoDb()
+    app.mongodb = mongodb
     yield
-
+    app.mongodb.close()
 
 app = FastAPI(lifeSpan = lifeSpan)
 
